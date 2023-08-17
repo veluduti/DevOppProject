0c159a7d9d47360e6237f2378afc1c217b5e58c6
Admin <Admin@DESKTOP-TI6JGS8>
2023-08-17 15:57:28 +0530

    commit

---
+++ validate/src/com/lbrce/devops/validate.java
@@ -0,0 +1,12 @@
+package com.lbrce.devops;
+
+public class validate {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int n1 = 225, n2 = 115, sum;
+		sum = n1 + n2;
+		System.out.println("The sum of numbers is: "+sum);
+	}
+
+}
